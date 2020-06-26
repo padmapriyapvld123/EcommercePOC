b0c2671fc4d3d19f8cd5ea2960f379565c6ef4b1
padmapriya.pvld@nineleaps.com <padmapriya.pvld@nineleaps.com>
2020-06-26 20:51:21 +0530

    Junit for Order Service

--- cassandra-init.sh
+++ cassandra-init.sh
@@ -1,10 +1,15 @@
 
-CQL="DROP keyspace cycling;
+CQL="DROP keyspace cycling1;
 
-CREATE KEYSPACE cycling
+CREATE KEYSPACE cycling1
 WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 3};
 
-CREATE TABLE cycling."product_supplier" (
+DELETE FROM system_schema.tables WHERE keyspace_name = 'cycling' AND table_name = 'order_items'; 
+DELETE FROM system_schema.columns WHERE keyspace_name = 'cycling' AND table_name = 'order_items'; 
+
+
+
+CREATE TABLE cycling1."product_supplier" (
 	productid varchar,
 	description varchar,
 	name varchar,
@@ -13,7 +18,9 @@ CREATE TABLE cycling."product_supplier" (
 	PRIMARY KEY (productid)
 );
 
-CREATE TABLE cycling."supplier" (
+
+
+CREATE TABLE cycling1."supplier" (
 	supplierid varchar,
 	email varchar,
 	name varchar,
@@ -21,12 +28,13 @@ CREATE TABLE cycling."supplier" (
 );
 
 
-CREATE TYPE cycling."item_entity"
+CREATE TYPE cycling1."item_entity"
 (product_id text,
 price double,
 quantity int);
 
-CREATE TABLE cycling."order_items"
+
+CREATE TABLE cycling1."order_items"
 (id text,
 date text,
 customer_name text,
